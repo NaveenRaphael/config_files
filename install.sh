d8d2b3f7bacec7f74f423c4caca6e46cd0be1f1f
Naveen-xubuntu <naveenbraphael@gmail.com>
2025-11-17 14:27:48 +0530

    modified the install script slightly

--- install.sh
+++ install.sh
@@ -2,15 +2,19 @@
 # This is not a script that does things automatically; it needs monitoring
 
 # git
+echo "Installing git"
 sudo apt install git
 
 # curl
+echo "Installing curl"
 sudo apt install curl
 
 # rust
+echo "Installing rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # helix editor; I am compiling from source because I usually get used to the dev build
+echo "Installing hx"
 mkdir coding
 cd coding
 git clone https://github.com/helix-editor/helix.git
@@ -21,19 +25,23 @@ echo "export HELIX_RUNTIME=/home/naveen/Documents/Naveen/helix/runtime" >> ~/.ba
 echo 'export EDITOR="hx"' >> ~/.bashrc
 
 # uv
+echo "Installing uv"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # miniforge
+echo "Installing miniforge"
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh
 # Do the miniforge setup manually
 
 # fzf
+echo "Installing fzf"
 sudo apt install fzf
 echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >>~/.bashrc
 echo 'eval "$(fzf --bash)"' >>~/.bashrc
 
 # zoxide
+echo "Installing zoxide"
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 echo 'eval "$(zoxide init bash --cmd cd)"' >> ~/.bashrc
 
@@ -42,17 +50,22 @@ echo 'eval "$(zoxide init bash --cmd cd)"' >> ~/.bashrc
 #
 
 # ffmpeg
+echo "Installing ffmpeg"
 sudo apt install ffmpeg
 
 # fd
+echo "Installing fd"
 sudo apt install fd-find
 ln -s $(which fdfind) ~/.local/bin/fd
 
 # ripgrep
+echo "Installing ripgrep"
 sudo apt install ripgrep
 
 # yazi
+echo "Installing yazi"
 cargo install --force yazi-build
 
 # flatpak
+echo "Installing flatpak"
 sudo apt install flatpak
